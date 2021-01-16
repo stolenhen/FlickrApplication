c315d6ecd3d90abfe44064c2949053832ba0782e
Ivan <stolenhen@gmail.com>
2021-01-16 17:59:54 +0300

    Initial Commit

---
+++ FlickrApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlickrApplication
+//
+//  Created by stolenhen on 16.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlickrApplication/FlickrApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApplicationApp.swift
+//  FlickrApplication
+//
+//  Created by stolenhen on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
